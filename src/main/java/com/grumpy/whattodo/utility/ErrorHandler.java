e9c3250779b01ad2cc19d1bbadbfe6b249caf2a0
Arnold Olszok | hybris <arnold.olszok@hybris.com>
2015-04-29 17:05:43 +0200

    error handler

---
+++ src/main/java/com/grumpy/whattodo/utility/ErrorHandler.java
@@ -0,0 +1,27 @@
+package com.grumpy.whattodo.utility;
+
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
+
+import org.springframework.web.client.HttpClientErrorException;
+
+public class ErrorHandler {
+
+    public static void handleResponseStatusCode(HttpClientErrorException clientError) {
+	switch (clientError.getStatusCode().value()) {
+	    case 400:
+		throw new BadRequestException();
+	    case 401:
+		// TODO: Exception for UnAuthorized
+		throw new InternalServerErrorException();
+	    case 403:
+		throw new ForbiddenException();
+	    case 404:
+		throw new NotFoundException();
+
+	}
+    }
+
+}
