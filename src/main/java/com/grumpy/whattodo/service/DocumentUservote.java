9f4534c10c3f6ad95f05e53481a727d7cee6d249
Tomasz Zasoń <tomasz.zason@hybris.com>
2015-04-30 12:50:19 +0200

    Userservice v3

--- src/main/java/com/grumpy/whattodo/service/DocumentUservote.java
+++ src/main/java/com/grumpy/whattodo/service/DocumentUservote.java
@@ -1,14 +1,17 @@
 package com.grumpy.whattodo.service;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.grumpy.whattodo.api.generated.Uservote;
 
 
 public class DocumentUservote extends Uservote
 {
-/*	@javax.validation.constraints.Pattern(regexp = "^.+")
-	@javax.validation.constraints.NotNull
-	@JsonIgnore*/
+	/*
+	 * @javax.validation.constraints.Pattern(regexp = "^.+")
+	 * 
+	 * @javax.validation.constraints.NotNull
+	 * 
+	 * @JsonIgnore
+	 */
 	private DocumentMetaData _metaData;
 
 	private String whatToDoFk;
@@ -40,13 +43,15 @@ public class DocumentUservote extends Uservote
 		this.whatToDoFk = whatToDoFk;
 	}
 
-/*	public DocumentMetaData getMetadata()
-	{
-		return _metaData;
-	}
-
-	public void setMetadata(final DocumentMetaData _metaData)
-	{
-		this._metaData = _metaData;
-	}*/
+	/*
+	 * public DocumentMetaData getMetadata()
+	 * {
+	 * return _metaData;
+	 * }
+	 * 
+	 * public void setMetadata(final DocumentMetaData _metaData)
+	 * {
+	 * this._metaData = _metaData;
+	 * }
+	 */
 }
